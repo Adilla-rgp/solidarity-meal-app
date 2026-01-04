7afbafb8da09737fdae6c9a77d4d4d618aefe8a0
Adilla-rgp <adillarobertagomespereira@gmail.com>
2026-01-04 14:43:04 -0300

    fix(layout): remover espaço indevido entre sidebar e conteúdo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,16 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "@/app/globals.css";
-import { DoadorProvider } from "@/app/contexts/DoadorContext";
-import { BeneficiarioProvider } from "@/app/contexts/BeneficiarioContext";
-import { AuthProvider } from "./contexts/autenticacaoContext";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Prato Solidário",
-  description: "Plataforma de doações de aliemntos",
-};
+import "./globals.css";
+import { AuthProvider } from "@/app/contexts/autenticacaoContext";
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {/* Envolvemos toda a aplicação com os três Providers */}
-        <AuthProvider>
-          <DoadorProvider>
-            <BeneficiarioProvider>
-              {children}
-            </BeneficiarioProvider>
-          </DoadorProvider>
-        </AuthProvider>
+    <html lang="pt-BR">
+      <body>
+        <AuthProvider>{children}</AuthProvider>
       </body>
-    </html >
+    </html>
   );
-} 
+}
